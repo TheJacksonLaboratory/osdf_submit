80a05ef611832e9863ef14c0e67c5178c6d7d9fe
cometsong <benjamin@cometsong.net>
2015-11-30 09:54:45 -0500

    mod project.py to match loading styles of other scripts.

--- submit_scripts/project.py
+++ submit_scripts/project.py
@@ -2,15 +2,15 @@
 
 import json
 import logging
-from pprint import pprint
-import yaml
 
-from cutlass_utils import save_if_valid, load_string_from_file
+from cutlass_utils import save_if_valid, \
+    load_string_from_file, \
+    load_config_from_file
 from cutlass import iHMPSession
 from cutlass import Project as OSDFNode
 
 # Project info yaml file name
-yaml_info_file = '../data_files/project_info.yaml'
+info_file = '../data_files/project_info.yaml'
 
 # load username, password from files
 username = load_string_from_file('../auth/username.txt')
@@ -19,18 +19,19 @@ password = load_string_from_file('../auth/password.txt')
 session = iHMPSession(username, password)
 
 print("Required fields: {}".format(OSDFNode.required_fields()))
-node = OSDFNode()
 
-# load info from yaml_info_file
-node_info = yaml.load(open(yaml_info_file))
+# load project info from info_file
+config = load_config_from_file(info_file)
+for node_info in config:
+    node = OSDFNode()
 
-node.name = node_info['name']
-node.description = node_info['description']
-node.center = node_info['center']
-node.contact = node_info['contact']
-node.srp_id = node_info['srp_id']
-node.tags = node_info['tags']
-node.mixs = node_info['mixs']
+    node.name = node_info['name']
+    node.description = node_info['description']
+    node.center = node_info['center']
+    node.contact = node_info['contact']
+    node.srp_id = node_info['srp_id']
+    node.tags = node_info['tags']
+    node.mixs = node_info['mixs']
 
-# print(node.to_json(indent=2))
-save_if_valid(node, OSDFNode)
+    # print(node.to_json(indent=2))
+    save_if_valid(node, OSDFNode)
